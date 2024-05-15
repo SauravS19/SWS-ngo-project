14278e693cebfa6d04bc890e180e6f4d17273895
Abhay Aswal <abhayaswal939@gamil.com>
2024-05-16 00:17:08 +0530

    resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-
-import { Route, Routes } from 'react-router-dom'
 import React, { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 const Navbar = React.lazy(()=>import("./components/Navbar"))
 const About = React.lazy(()=> import("./components/pages/About"))
@@ -8,16 +7,15 @@ const Contact = React.lazy(()=> import("./components/pages/Contact"))
 const Home = React.lazy(()=> import("./components/pages/Home"))
 const Blogs = React.lazy(()=> import("./components/pages/Blogs"))
 const Event = React.lazy(()=> import("./components/pages/Event"))
-const Hero = React.lazy(()=> import("./components/Hero"))
+
 function App() {
 
   return (<>
-   <Navbar></Navbar>
+  <Navbar/>
       <Routes> 
         <Route path='/' element={<Suspense fallback={"loading"}><Home/></Suspense>}/>
         <Route path='/about' element={<Suspense fallback={"loading"}><About/></Suspense>}/>
         <Route path='/contact' element={<Suspense fallback={"loading"}><Contact/></Suspense>}/>
-        <Route path='/home' element={<Suspense fallback={"loading"}><Home/></Suspense>}/>
         <Route path='/blogs' element={<Suspense fallback={"loading"}><Blogs/></Suspense>}/>
         <Route path='/events' element={<Suspense fallback={"loading"}><Event/></Suspense>}/>
      </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,8 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-
-// const Navbar = () => {
-//   return (
-//     <div className="fixed -[999] w-full px-20 py-1.5 flex justify-between items-center z-50 bg-white">
-//        <div className='logo'>
-//       <div className='font-semibold uppercase text-5xl'>SWS</div>
-//        </div>
-//        <div className="links flex gap-10">
-//             <Link className='text-md capitalize font-semibold' to={"/home"}>Home</Link>
-//             <Link className='text-md capitalize font-semibold' to={"/about"}>About us</Link>
-//             <Link className='text-md capitalize font-semibold' to={"/blogs"}>Blogs</Link>
-//             <Link className='text-md capitalize font-semibold' to={"/events"}>Events</Link>
-//             <Link className='text-md capitalize font-semibold' to={"/contact"}>contact</Link>
-//        </div>
-//     </div>
-//   )
-// }
-
 // export default Navbar;
 import React, { useState } from 'react';
 import {  AiFillFacebook, AiFillInstagram, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
-import logo from '../assets/youtube.png'
 
-function Navbar() {
+const  Navbar = ()=> {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const showSidebar = () => {
